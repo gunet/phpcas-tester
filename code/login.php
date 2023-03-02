75b6c0b24604310da34c0b5e070fff87ff2a54b9
kkalev <kkalev@gmail.com>
2023-03-02 16:15:46 +0200

    cosmetic changes

--- code/login.php
+++ code/login.php
@@ -3,26 +3,25 @@
 require_once('vendor/autoload.php');
 require_once('include/GuestAuth.class.php');
 require_once('include/CasAuth.class.php');
-
   
-    $casAuth = new CasAuth(true);
+$casAuth = new CasAuth(true);
 
-    if ($casAuth->isAuthenticated()) {
-        echo "User Authenticated<br>\n";
-        // Get the user's attributes
-        if ($casAuth->hasAttributes()) {
-            $attributes = $casAuth->getAttributes();
+if ($casAuth->isAuthenticated()) {
+    echo "User Authenticated<br>\n";
+    // Get the user's attributes
+    if ($casAuth->hasAttributes()) {
+        $attributes = $casAuth->getAttributes();
 
-            echo "attributes: " . json_encode($attributes);
+        echo "attributes: " . json_encode($attributes);
 
-            // Print all the attributes
-            foreach ($attributes as $key => $value) {
-                echo "$key: $value<br>";
-            }
+        // Print all the attributes
+        foreach ($attributes as $key => $value) {
+            echo "$key: $value<br>";
         }
     }
-    else {
-        $casAuth->login();
-    }
+}
+else {
+    $casAuth->login();
+}
 ?>
 
